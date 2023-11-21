2fc9ebf065498cbeba95a514da0e15accc1073b7
Corey Chiu <qiuyu@qiuyu.dev>
2023-11-21 12:56:41 +0800

    change to qiuyu

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -5,7 +5,7 @@ addons.setConfig({
   theme: {
     ...themes.dark,
     brandImage: 'https://hamishw.com/icon.svg',
-    brandTitle: 'QiuYu Components',
+    brandTitle: 'Hamish Williams Components',
     brandUrl: 'https://hamishw.com',
   },
 });
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -145,7 +145,7 @@ export const Navbar = () => {
         <a
           data-navbar-item
           className={styles.logo}
-          aria-label="QiuYu, Designer"
+          aria-label="Hamish Williams, Designer"
           onClick={handleMobileNavClick}
         >
           <Monogram highlight />
